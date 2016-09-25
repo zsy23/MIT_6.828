e8b458e012fe76c9fdbc55c4ea389dc1140a40d4
ShiyuZhang21 <zsymemorier@gmail.com>
2016-09-25 15:36:09 +0800

    merge lab4 into lab5(2)

--- kern/env.c
+++ kern/env.c
@@ -386,12 +386,13 @@ load_icode(struct Env *e, uint8_t *binary)
 void
 env_create(uint8_t *binary, enum EnvType type)
 {
+	// LAB 3: Your code here.
 	// If this is the file server (type == ENV_TYPE_FS) give it I/O privileges.
 	// LAB 5: Your code here.
-	// struct Env *e;
-	// env_alloc(&e, 0);
-	// load_icode(e, binary);
-	// e->env_type = type;
+	struct Env *e;
+	env_alloc(&e, 0);
+	load_icode(e, binary);
+	e->env_type = type;
 }
 
 //
